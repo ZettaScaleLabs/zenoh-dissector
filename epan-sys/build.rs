53afc8d031950554d0031c419671af6a15730e1c
yuanyuyuan <az6980522@gmail.com>
2024-03-07 15:32:07 +0800

    Improve the building process

--- epan-sys/build.rs
+++ epan-sys/build.rs
@@ -2,11 +2,19 @@
 extern crate bindgen;
 
 use anyhow::Result;
+use lazy_static::lazy_static;
 use std::env;
 use std::path::PathBuf;
 
-const WIRESHARK_VERSION: &str = "4.2.3";
-const WIRESHARK_SOURCE_DIR: &str = "wireshark";
+lazy_static! {
+    static ref WIRESHARK_VERSION: String = "4.2.3".to_string();
+    static ref WIRESHARK_SOURCE_DIR: PathBuf = PathBuf::from(format!(
+        "{}/wireshark-{}",
+        env::var("CARGO_MANIFEST_DIR").unwrap(),
+        *WIRESHARK_VERSION
+    ));
+    static ref WIRESHARK_BUILD_DIR: PathBuf = WIRESHARK_SOURCE_DIR.join("build");
+}
 
 fn main() -> Result<()> {
     // If we are in docs.rs, there is no need to actually link.
@@ -14,8 +22,7 @@ fn main() -> Result<()> {
         return Ok(());
     }
 
-    // By default, we will just use a pre-generated bindings.rs file. If this feature is turned
-    // on, we'll re-generate the bindings at build time.
+    // Re-generate the bindings at build time.
     #[cfg(feature = "bindgen")]
     generate_bindings()?;
 
@@ -29,31 +36,33 @@ fn link_wireshark() -> Result<()> {
         return Ok(());
     }
 
-    // Default wireshark libraray installed on macos
-    #[cfg(target_os = "macos")]
-    println!(
-        "cargo:rustc-link-search=native={}",
-        "/Applications/Wireshark.app/Contents/Frameworks"
-    );
-
-    // Specify the wireshark library directory by the environmental variable
+    // Follow the given environmental variable WIRESHARK_LIB_DIR
     println!("cargo:rerun-if-env-changed=WIRESHARK_LIB_DIR");
     if let Ok(libws_dir) = env::var("WIRESHARK_LIB_DIR") {
         println!("cargo:rustc-link-search=native={}", libws_dir);
-    } else {
-        // We need to build the wireshark from source for the linking
-        #[cfg(target_os = "windows")]
-        {
-            env::set_var("WIRESHARK_BASE_DIR", "C:\\Development");
-            env::set_var("PLATFORM", "win64");
+    }
 
-            // Download and build wireshark.lib
-            download_wireshark()?;
-            let dst = build_wireshark();
-            // Default wireshark libraray installed on windows
-            // println!( "cargo:rustc-link-search=native={}", "C:\\Program Files\\Wireshark");
-            println!("cargo:rustc-link-search=native={}", dst.to_string_lossy());
+
+    // Default wireshark libraray installed on macos
+    #[cfg(target_os = "macos")]
+    {
+        let macos_wireshark_library = "/applications/wireshark.app/contents/frameworks";
+        if !pathbuf::from(macos_wireshark_library).exists() {
+            panic!("wireshark library not found at {macos_wireshark_library}");
         }
+        println!("cargo:rustc-link-search=native={macos_wireshark_library}");
+    }
+
+    #[cfg(target_os = "windows")]
+    {
+        if !WIRESHARK_BUILD_DIR.exists() {
+            download_wireshark(true)?;
+            build_wireshark();
+        }
+        println!(
+            "cargo:rustc-link-search=native={}",
+            WIRESHARK_BUILD_DIR.join("wireshark").to_string_lossy()
+        );
     }
 
     println!("cargo:rustc-link-lib=dylib=wireshark");
@@ -74,63 +83,83 @@ fn generate_bindings() -> Result<()> {
             }
         }
         Err(_) => {
-            download_wireshark()?;
+            download_wireshark(true)?;
+            build_wireshark();
 
+            // ws_version.h is under wireshark-{ver}/build
+            builder = builder.clang_arg(format!("-I{}", WIRESHARK_BUILD_DIR.to_string_lossy()));
+
+            // general header files are under wireshark-{ver} and wireshark-{ver}/include
+            builder = builder.clang_arg(format!(
+                "-I{}",
+                WIRESHARK_SOURCE_DIR.join("include").to_string_lossy()
+            ));
+            builder = builder.clang_arg(format!("-I{}", WIRESHARK_SOURCE_DIR.to_string_lossy()));
+
+            // glib-2.0 is installed under vcpkg
             #[cfg(target_os = "windows")]
-            {
-                env::set_var("WIRESHARK_BASE_DIR", "C:\\Development");
-                env::set_var("PLATFORM", "win64");
-            }
-            let dst = build_wireshark();
+            env::set_var(
+                "PKG_CONFIG_PATH",
+                "C:\\Development\\wireshark-x64-libs-4.2\\vcpkg-export-20231017-1-x64-windows-ws\\installed\\x64-windows\\lib\\pkgconfig",
+            );
 
-            let mut ws_headers_path = dst;
-            ws_headers_path.push("include");
-            ws_headers_path.push("wireshark");
-
+            // header files for glib-2.0
             let glib = pkg_config::Config::new().probe("glib-2.0")?;
             for path in glib.include_paths {
                 builder = builder.clang_arg(format!("-I{}", path.to_string_lossy()));
             }
-            builder = builder.clang_arg(format!("-I{}", ws_headers_path.to_string_lossy()));
         }
     }
 
     let bindings = builder.generate()?;
     let out_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+
+    #[cfg(not(target_os = "windows"))]
     bindings.write_to_file(out_path.join("bindings.rs"))?;
+    #[cfg(target_os = "windows")]
+    bindings.write_to_file(out_path.join("bindings_windows.rs"))?;
 
     Ok(())
 }
 
-#[cfg(any(feature = "bindgen", target_os = "windows"))]
-fn download_wireshark() -> Result<()> {
+fn download_wireshark(skip_existing: bool) -> Result<()> {
+    if skip_existing && WIRESHARK_SOURCE_DIR.exists() {
+        return Ok(());
+    }
+
     use tar::Archive;
     use xz2::read::XzDecoder;
 
-    let url = format!("https://2.na.dl.wireshark.org/src/wireshark-{WIRESHARK_VERSION}.tar.xz");
+    let url = format!(
+        "https://2.na.dl.wireshark.org/src/wireshark-{}.tar.xz",
+        *WIRESHARK_VERSION
+    );
 
     let response = reqwest::blocking::get(url)?;
     let bytes = response.bytes()?.to_vec();
     let readable = XzDecoder::new(bytes.as_slice());
     let mut archive = Archive::new(readable);
     archive.unpack(".")?;
-    if std::path::Path::new(WIRESHARK_SOURCE_DIR).exists() {
-        std::fs::remove_dir_all(WIRESHARK_SOURCE_DIR)?;
+    if WIRESHARK_SOURCE_DIR.exists() {
+        std::fs::remove_dir_all(&*WIRESHARK_SOURCE_DIR)?;
     }
     std::fs::rename(
-        format!("wireshark-{WIRESHARK_VERSION}"),
-        WIRESHARK_SOURCE_DIR,
+        format!("wireshark-{}", *WIRESHARK_VERSION),
+        &*WIRESHARK_SOURCE_DIR,
     )?;
     Ok(())
 }
 
-fn build_wireshark() -> PathBuf {
+fn build_wireshark() {
     #[cfg(target_os = "windows")]
     {
+        // This installs the vcpkg under C:\\Development\wireshark-x64-libs-{ver}
         env::set_var("WIRESHARK_BASE_DIR", "C:\\Development");
         env::set_var("PLATFORM", "win64");
     }
-    cmake::Config::new(WIRESHARK_SOURCE_DIR)
+
+    // The generated files will be directed to WIRESHARK_BUILD_DIR instead of the default OUT_DIR, enhancing reusability.
+    let _ = cmake::Config::new(&*WIRESHARK_SOURCE_DIR)
         .define("BUILD_androiddump", "OFF")
         .define("BUILD_capinfos", "OFF")
         .define("BUILD_captype", "OFF")
@@ -164,5 +193,7 @@ fn build_wireshark() -> PathBuf {
         .define("ENABLE_LIBXML2", "OFF")
         .define("ENABLE_OPUS", "OFF")
         .define("ENABLE_SINSP", "OFF")
-        .build()
+        .out_dir(&*WIRESHARK_SOURCE_DIR)
+        .very_verbose(true)
+        .build();
 }
