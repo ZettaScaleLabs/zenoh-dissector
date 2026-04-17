ec585cfb99e53efb6cfd9ff9e28a48340398c3c9
yuanyuyuan <az6980522@gmail.com>
2026-04-17 23:08:38 +0800

    fix: add WIRESHARK_BUILD_DIR to clang include paths for bindgen (ws_version.h is cmake-generated)

--- epan-sys/build.rs
+++ epan-sys/build.rs
@@ -336,7 +336,8 @@ fn generate_bindings() -> Result<()> {
                     "-I{}",
                     WIRESHARK_SOURCE_DIR.join("include").to_string_lossy()
                 ))
-                .clang_arg(format!("-I{}", WIRESHARK_SOURCE_DIR.to_string_lossy()));
+                .clang_arg(format!("-I{}", WIRESHARK_SOURCE_DIR.to_string_lossy()))
+                .clang_arg(format!("-I{}", WIRESHARK_BUILD_DIR.to_string_lossy()));
 
             #[cfg(target_os = "windows")]
             {
